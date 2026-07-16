d74055a417e5b4de80b769a92d9423956a6cb4ad
Allard Buijze <allard.buijze@axoniq.io>
2026-07-16 16:16:34 +0200

    Address PR review feedback on externalized code samples

--- docs/reference-guide/modules/messaging-concepts/examples/messagingconcepts/messagecorrelation/Configuration.java
+++ docs/reference-guide/modules/messaging-concepts/examples/messagingconcepts/messagecorrelation/Configuration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2010-2026. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package messagingconcepts.messagecorrelation;
 
 import org.axonframework.messaging.core.correlation.CorrelationDataProvider;
@@ -5,8 +20,10 @@ import org.axonframework.messaging.core.correlation.MessageOriginProvider;
 import org.axonframework.messaging.core.correlation.MultiCorrelationDataProvider;
 import org.axonframework.messaging.core.correlation.SimpleCorrelationDataProvider;
 
+import java.util.List;
+
 // tag::multi-provider[]
-import java.util.List;public class Configuration {
+public class Configuration {
 
     public CorrelationDataProvider customCorrelationDataProviders() {
         return new MultiCorrelationDataProvider(
--- docs/reference-guide/modules/messaging-concepts/examples/messagingconcepts/processingcontext/TransactionInterceptor.java
+++ docs/reference-guide/modules/messaging-concepts/examples/messagingconcepts/processingcontext/TransactionInterceptor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2010-2026. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package messagingconcepts.processingcontext;
 
 import org.axonframework.messaging.commandhandling.CommandMessage;
@@ -9,8 +24,8 @@ import org.axonframework.messaging.core.unitofwork.ProcessingContext;
 
 // tag::transaction-interceptor[]
 // Note that this is an advanced example!
-// Axon Framework typically deals with transaction and connection details for you, so you don't have too.
-// That makes thi example useful ONLY if you are using a storage solution that is not supported by Axon Framework out of the box.
+// Axon Framework typically deals with transaction and connection details for you, so you don't have to.
+// That makes this example useful ONLY if you are using a storage solution that is not supported by Axon Framework out of the box.
 public class TransactionInterceptor implements MessageHandlerInterceptor<CommandMessage> {
 
     private static final ResourceKey<Transaction> TX_KEY = ResourceKey.withLabel("Transaction");
