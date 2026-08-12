691c4e68627531935112287c2489685cd3e938cd
Steven van Beelen <steven.vanbeelen@axoniq.io>
2026-08-12 09:50:02 +0200

    Expand migration path to show use of ForcedEntityCreator
    Expand migration path to show use of ForcedEntityCreator

---
+++ docs/reference-guide/modules/migration/examples/migration/paths/aggregates/index/forcedentitycreator/GiftCard.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2010-2026. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package migration.paths.aggregates.index.forcedentitycreator;
+
+import org.axonframework.eventsourcing.annotation.EventSourcedEntity;
+import org.axonframework.eventsourcing.annotation.EventSourcingHandler;
+import org.axonframework.eventsourcing.annotation.reflection.ForcedEntityCreator;
+import org.axonframework.eventsourcing.annotation.reflection.InjectEntityId;
+import org.axonframework.messaging.commandhandling.annotation.CommandHandler;
+import org.axonframework.messaging.eventhandling.gateway.EventAppender;
+
+// tag::forced-entity-creator-create-if-missing[]
+@EventSourcedEntity
+public class GiftCard {
+
+    private String cardId;
+    private boolean issued;
+
+    @ForcedEntityCreator
+    public GiftCard(@InjectEntityId String cardId) {
+        this.cardId = cardId;
+    }
+
+    @CommandHandler
+    public void handle(IssueGiftCard cmd, EventAppender eventAppender) {
+        if (issued) {
+            throw new IllegalStateException("GiftCard already issued");
+        }
+        eventAppender.append(new GiftCardIssued(cardId, cmd.amount()));
+    }
+
+    @EventSourcingHandler
+    public void on(GiftCardIssued event) {
+        this.issued = true;
+    }
+}
+// end::forced-entity-creator-create-if-missing[]
